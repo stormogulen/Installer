b5fee6c541a2eb804351709f0fff91a40e855b59
Ulf Ejlertsson <ulf.ejlertsson@gmail.com>
2023-08-14 09:29:37 +0200

    Updated to handle scripts

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,41 @@
 //use std::error::Error;
-use std::fs;
+//use std::fs;
 use std::io::{self, Error, ErrorKind};
 
 type Task = Box<dyn FnOnce() -> Result<(), Error>>;
 type Rollback = Box<dyn FnOnce() -> Result<(), Error>>;
+type Script = Box<dyn FnOnce() -> Result<(), Error>>;
 
 struct Transaction {
     execute_fn: Option<Task>,
     rollback_fn: Option<Rollback>,
+    script_fn: Option<Script>,
 }
 
 impl Transaction {
-    fn new<E, R>(execute_fn: E, rollback_fn: R) -> Self
+    fn new<E, R, S>(execute_fn: E, rollback_fn: R, script_fn: S) -> Self
     where
         E: Fn() -> Result<(), Error> + 'static,
         R: Fn() -> Result<(), Error> + 'static,
+        S: FnOnce() -> Result<(), Error> + 'static,
     {
         Transaction {
             execute_fn: Some(Box::new(execute_fn)),
             rollback_fn: Some(Box::new(rollback_fn)),
+            script_fn: Some(Box::new(script_fn)),
         }
     }
+
+    // fn new<E, R>(execute_fn: E, rollback_fn: R) -> Self
+    // where
+    //     E: Fn() -> Result<(), Error> + 'static,
+    //     R: Fn() -> Result<(), Error> + 'static,
+    // {
+    //     Transaction {
+    //         execute_fn: Some(Box::new(execute_fn)),
+    //         rollback_fn: Some(Box::new(rollback_fn)),
+    //     }
+    // }
 
     fn execute_fn(&mut self) -> Result<(), Error> {
         //(self.execute_fn)()
@@ -33,6 +48,11 @@ impl Transaction {
         let rollback_fn = self.rollback_fn.take().expect("No rollback_fn found");
         rollback_fn()
     }
+
+    fn script_fn(&mut self) -> Result<(), Error> {
+        let script_fn = self.script_fn.take().expect("No script_fn found");
+        script_fn()
+    }
 }
 
 fn main() {
@@ -40,10 +60,12 @@ fn main() {
         Transaction::new(
             Box::new(|| copy_file_task()),
             Box::new(|| copy_file_rollback()),
+            Box::new(|| run_script_task()),
         ),
         Transaction::new(
             Box::new(|| create_directory_task()),
             Box::new(|| create_directory_rollback()),
+            Box::new(|| create_run_script_task()),
         ),
     ];
 
@@ -55,6 +77,15 @@ fn main() {
             Ok(()) => {}
             Err(err) => {
                 println!("Task execution failed: {}", err);
+                rollback_required = true;
+                break;
+            }
+        }
+
+        match task.script_fn() {
+            Ok(()) => {}
+            Err(err) => {
+                println!("Run script failed: {}", err);
                 rollback_required = true;
                 break;
             }
@@ -84,6 +115,10 @@ fn copy_file_rollback() -> Result<(), Error> {
     Ok(())
 }
 
+fn run_script_task() -> Result<(), Error> {
+    Ok(())
+}
+
 fn create_directory_task() -> Result<(), Error> {
     println!("Creating directory");
     // Perform directory creation logic here
@@ -93,6 +128,10 @@ fn create_directory_task() -> Result<(), Error> {
 fn create_directory_rollback() -> Result<(), Error> {
     println!("Rolling back directory creation");
     // Perform rollback logic here
+    Ok(())
+}
+
+fn create_run_script_task() -> Result<(), Error> {
     Ok(())
 }
 
@@ -129,6 +168,20 @@ mod tests {
         // Test rolling back directory creation (replace with actual test logic)
         let result = create_directory_rollback();
         assert!(result.is_ok()); // Check if the rollback completed successfully
+        Ok(())
+    }
+
+    #[test]
+    fn test_run_script_task() -> Result<(), Box<Error>> {
+        let result = run_script_task();
+        assert!(result.is_ok()); // Check if the task completed successfully
+        Ok(())
+    }
+
+    #[test]
+    fn test_create_run_script_task() -> Result<(), Box<Error>> {
+        let result = create_run_script_task();
+        assert!(result.is_ok()); // Check if the task completed successfully
         Ok(())
     }
 }
